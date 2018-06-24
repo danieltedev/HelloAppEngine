e180d7265dc3f29f5fd26f2e1e5f83d360e2e712
daniel <daniel.t.e.n@hotmail.com>
2018-06-24 17:46:58 +0000

    show menssage

--- src/test/java/com/example/HelloAppEngine/HelloAppEngineApplicationTests.java
+++ src/test/java/com/example/HelloAppEngine/HelloAppEngineApplicationTests.java
@@ -4,13 +4,20 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.web.bind.annotation.*;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@RestController
 public class HelloAppEngineApplicationTests {
 
 	@Test
 	public void contextLoads() {
 	}
+	
+	@GetMapping("/")
+    public String hello() {
+        return "Hello Spring Boot!";
+    }
 
 }
